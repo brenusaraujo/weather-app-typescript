64e6cbc57eaf176a9c2e8f26ad1f9d788e9c88dc
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-19 10:49:32 +0000

    adiciona eslint

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      <section className="">HelloWorld</section>
+      <section className="bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded">
+        HelloWorld
+      </section>
     </main>
   )
 }
