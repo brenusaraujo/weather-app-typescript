d963d3422861eccb5d0fa5fed5be7a6a79295308
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-19 10:33:11 +0000

    alteraçoes no package.json

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      <section className="">HelloWorld</section>
     </main>
   )
 }
